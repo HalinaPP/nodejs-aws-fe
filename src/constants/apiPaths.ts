0b41ee0ebae82368df4b600821bbba6942df48fa
halinapp <galunikipp@gmail.com>
2020-11-16 00:19:03 +0300

    feat: set path, fix content headers

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://tgyctvcbw8.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://tgyctvcbw8.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://tgyctvcbw8.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://vwe83ahg22.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://tgyctvcbw8.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
